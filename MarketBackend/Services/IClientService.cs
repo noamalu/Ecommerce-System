b1db86934bcf9875eb63ca624c6e41628b3ef00f
noamalu <noamalu@post.bgu.ac.il>
2024-05-10 08:55:35 +0300

    rename proj directory

---
+++ MarketBackend/Services/IClientService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketBackend.Services
+{
+    public interface IClientService
+    {
+        void register(string username, string password);
+        void enterAsGuest();
+        void purchaseCart(int id);
+        void createStore(int id);
+        void resToStoreManageReq();
+        void resToStoreOwnershipReq();
+        void logoutClient(int id);
+        void removeFromCart(int clientId, int productId);
+        void viewCart(int id);
+        void addToCart(int clientId, int productId);
+
+        void loginClient(string username, string password);
+        void exitGuest();
+        void browseGuest();
+        void getPurchaseHistory(int id);
+    }
+}
