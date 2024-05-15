6ed127b4aea02bdaab3556b0dedfd1b6f719b56c
ol-ozn <ol.oznovich@gmail.com>
2024-05-15 14:03:23 +0300

    added canGetHistory

--- MarketBackend/Domain/Market_Client/Role.cs
+++ MarketBackend/Domain/Market_Client/Role.cs
@@ -29,31 +29,32 @@ namespace MarketBackend.Domain.Market_Client
         public void addAppointee(Member appToAdd) {  appointees.Add(appToAdd);}
         public void removeAppointee(Member appToRem) { appointees.Add(appToRem); }
 
-        public virtual bool canAddProduct() { return role.canAddProduct(); }
-        public virtual bool canRemoveProduct() { return role.canRemoveProduct(); }
-        public virtual bool canOpenStore() { return role.canOpenStore(); }
-        public virtual bool canCloseStore() { return role.canCloseStore(); }
-        public virtual bool canUpdateProductPrice() { return role.canUpdateProductPrice(); }
-        public virtual bool canUpdateProductDiscount() { return role.canUpdateProductDiscount(); }
-        public virtual bool canUpdateProductQuantity() { return role.canUpdateProductQuantity(); }
-        public virtual bool canAddStaffMember() { return role.canAddStaffMember(); }
-        public virtual bool canRemoveStaffMember() { return role.canRemoveStaffMember(); }
+        public bool canAddProduct() { return role.canAddProduct(); }
+        public bool canRemoveProduct() { return role.canRemoveProduct(); }
+        public bool canOpenStore() { return role.canOpenStore(); }
+        public bool canCloseStore() { return role.canCloseStore(); }
+        public bool canUpdateProductPrice() { return role.canUpdateProductPrice(); }
+        public bool canUpdateProductDiscount() { return role.canUpdateProductDiscount(); }
+        public bool canUpdateProductQuantity() { return role.canUpdateProductQuantity(); }
+        public bool canAddStaffMember() { return role.canAddStaffMember(); }
+        public bool canRemoveStaffMember() { return role.canRemoveStaffMember(); }
 
-        public virtual bool hasPermission(Permission permission)
+        public bool canGetHistory() { return role.canGetHistory(); }
+        public bool hasPermission(Permission permission)
         {
             return role.hasPermission(permission);
         }
 
-        public virtual void addPermission(Permission permission)
+        public void addPermission(Permission permission)
         {
             role.addPermission(permission);
         }
-        public virtual void removePermission(Permission permission)
+        public void removePermission(Permission permission)
         {
             role.removePermission(permission);
         }
 
-        public virtual IReadOnlyCollection<Permission> getPermissions()
+        public IReadOnlyCollection<Permission> getPermissions()
         {
             return role.getPermissions();
         }
