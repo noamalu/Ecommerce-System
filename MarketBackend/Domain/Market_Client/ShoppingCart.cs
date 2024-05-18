f93da2f31dbc41ad65cd901163141218bffb919b
noamalu <noamalu@post.bgu.ac.il>
2024-05-18 12:37:34 +0300

    purchace prep work

--- MarketBackend/Domain/Market_Client/ShoppingCart.cs
+++ MarketBackend/Domain/Market_Client/ShoppingCart.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarketBackend.DAL;
 using MarketBackend.Services.Interfaces;
 
 namespace MarketBackend.Domain.Models
@@ -10,9 +11,9 @@ namespace MarketBackend.Domain.Models
     {
         public int _shoppingCartId;
         private readonly IBasketRepository _basketRepository;
-        public ShoppingCart(int shoppingCartId,IBasketRepository basketRepository){
+        public ShoppingCart(int shoppingCartId){
             _shoppingCartId = shoppingCartId;
-            _basketRepository = basketRepository;
+            _basketRepository = BasketRepositoryRAM.GetInstance();
         }
 
         public void addToCart(int basketId, int productId, int quantity){
@@ -23,6 +24,16 @@ namespace MarketBackend.Domain.Models
         public void removeFromCart(int basketId, int productId, int quantity){
             Basket basket = _basketRepository.GetById(basketId);
             basket.RemoveFromBasket(productId, quantity);
+        }
+
+        public Dictionary<int, Basket> GetBaskets() //returns a dictionary of store id to productIdxQuantity
+        {
+            var baskets = _basketRepository.getBasketsByCartId(_shoppingCartId);
+            var retBaskets = new Dictionary<int, Basket>();
+            foreach (var basket in baskets) {
+                retBaskets.Add(basket._storeId, basket);
+            };
+            return retBaskets;
         }
     }
 }
