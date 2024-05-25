da96997c226dcb20f2d3743afb03f5b6f1ef201c
cohnofar <cohnofar@post.bgu.ac.il>
2024-05-25 21:12:56 +0300

    4 tests for add staff member

--- TestProject/Tests/UT/StoreTest.cs
+++ TestProject/Tests/UT/StoreTest.cs
@@ -12,47 +12,23 @@ namespace UnitTests
         private Client _owner;
         private Store _Store;
         private Product _p1;
-        private Product _p2;
-        private Product _p3;
-        private Product _p4;
 
         [TestInitialize]
         public void Initialize()
         {
             MarketService s = MarketService.GetInstance();
             ClientService c = ClientService.GetInstance();
-            MarketContext.GetInstance().Dispose();
-            MarketContext context = MarketContext.GetInstance();
             ClientManager CM = ClientManager.GetInstance();
             MarketManagerFacade MMF = MarketManagerFacade.GetInstance();
-            MarketManager MM = MarketManager.GetInstance();
-            var mockDeliverySystem = new Mock<IDeliverySystem>();
-            var mockPaymentSystem = new Mock<IPaymentSystem>();
-            mockDeliverySystem.Setup(d => d.Connect())
-             .Returns(true);
-            mockPaymentSystem.Setup(d => d.Connect())
-             .Returns(true);
             c.Register(2, "nofar", "12345", "nofar@gmail.com", 19);
             c.LoginClient(2, "nofar", "12345");
             int storeId= MMF.CreateStore(2, "shop1", "shop@gmail.com", "0502552798");
             _owner = CM.GetClientById(2);
             _Store = MMF.GetStore(storeId);
-            s.AddProduct("2", _Store.StoreId, "Ball",0, "this is a ball", 52.6, 80, Category.None.ToString(), new List<string> { "soccer", "basketball", "round" });
-            s.AddProduct("2",  _Store.StoreId, "Ball1",0, "this is a ball1", 52.6, 80, Category.Pockemon.ToString(), new List<string> { "basketball", "round", "Pockemon" });
-            s.AddProduct("2",  _Store.StoreId, "Ball2",0, "this is a ball2", 52.6, 80, Category.None.ToString(), new List<string>());
-            s.AddProduct("2",  _Store.StoreId, "Ball3",0, "this is a ball3", 52.6, 80, Category.Furnitures.ToString(), new List<string> { "table" });
-            _p1 = _shop.Products.ToList().Find((p) => p.Id == 11);
-            _p2 = _shop.Products.ToList().Find((p) => p.Id == 12);
-            _p3 = _shop.Products.ToList().Find((p) => p.Id == 13);
-            _p4 = _shop.Products.ToList().Find((p) => p.Id == 14);
-            s.Register("3", "nofar", "54321");
-            s.Register("4", "noa", "111111");
-            s.Register("5", "hadas", "22222");
-            s.Register("6", "joni", "111111");
-            s.Login("3", "nofar", "54321");
-            s.Login("4", "noa", "111111");
-            s.Login("5", "hadas", "22222");
-            s.Login("6", "joni", "111111");
+            _Store.AddProduct(_owner.Id, "Brush", "" , "Brush", 4784, "hair", 21, false);
+            _p1 = _Store.Products.ToList().Find((p) => p.ProductId == 11);
+            c.Register(3, "Noa", "54321", "nofar@gmail.com", 18);
+            c.LoginClient(3, "Noa", "54321");
         }
 
         [TestMethod()]
@@ -197,6 +173,44 @@ namespace UnitTests
         {
             Assert.ThrowsException<Exception>(() => _Store.UpdateProductQuantity(3, _p1.ProductId, 45555));
         }
+
+        [TestMethod()]
+        public void AddStaffMemberSuccess()
+        {
+            Role role = new Role(new StoreManagerRole(RoleName.Manager), (Member)_owner, _Store._storeId, 3);
+            _Store.AddStaffMember(3, role, _owner.Id);
+            Assert.IsTrue(_Store.roles.ContainsValue(role));
+
+        }
+
+        [TestMethod()]
+        public void AddStaffMemberFailUserNotExist()
+        {
+            Role role = new Role(new StoreManagerRole(RoleName.Manager), (Member)_owner, _Store._storeId, 3);
+            _Store.AddStaffMember(3, role, 17);
+            Assert.IsFalse(_Store.roles.ContainsValue(role));
+
+        }
+
+        [TestMethod()]
+        public void AddStaffMemberFailUserHasNoPermissions()
+        {
+            Role role = new Role(new StoreManagerRole(RoleName.Manager), (Member)_owner, _Store._storeId, 3);
+            _Store.AddStaffMember(3, role, 3);
+            Assert.IsFalse(_Store.roles.ContainsValue(role));
+        }
+
+        [TestMethod()]
+        public void AddStaffMemberFailAnotherFounder()
+        {
+            Role role = new Role(new Founder(RoleName.Founder), (Member)_owner, _Store._storeId, 3);
+            _Store.AddStaffMember(3, role, _owner.Id);
+            Assert.IsFalse(_Store.roles.ContainsValue(role));
+
+        }
+
+ 
+
 
 
 
