0c1c2726a8f4054a2c5fc62cd335e514249c5ab1
hadasprintz <92999292+hadasprintz@users.noreply.github.com>
2024-06-03 21:20:21 +0300

    fix tests

--- TestProject/Tests/UT/ClientTest.cs
+++ TestProject/Tests/UT/ClientTest.cs
@@ -8,24 +8,34 @@ namespace UnitTests
     [TestClass]
     public class ClientTest
     {
+        [TestInitialize]
+        public void SetUp()
+        {
+            ProductRepositoryRAM productRepositoryRAM = ProductRepositoryRAM.GetInstance();
+            productRepositoryRAM.Add(new Product(11, 0, "product", "Regular", "nice", 1.0, "fruit", 10, false));
+        }
+
+
+
         [TestMethod]
         public void TestAddToCart()
         {
             var client = new Guest(1); // Testing with Guest as an example
-            client.AddToCart(0, 100, 1); // basket, productId, quantity
+
+            client.AddToCart(0, 11, 10); // basket, productId, quantity
             var productsInBasket = BasketRepositoryRAM.GetInstance().getBasketsByCartId(client.Cart._shoppingCartId).Where(basket => basket._storeId == 0).FirstOrDefault()?.products;
-            Assert.AreEqual(1, productsInBasket?[100]);
+            Assert.AreEqual(10, productsInBasket?[11]);
         }
 
         [TestMethod]
         public void TestRemoveFromCart()
         {
             var client = new Guest(1);
-            client.AddToCart(0, 100, 1);
+            client.AddToCart(0, 11, 10);
             var basket = BasketRepositoryRAM.GetInstance().getBasketsByCartId(client.Cart._shoppingCartId).Where(basket => basket._storeId == 0).FirstOrDefault();
-            client.RemoveFromCart(basket._basketId, 100, 1);
+            client.RemoveFromCart(basket._basketId, 11, 10);
             var productsInBasket = basket.products;
-            Assert.AreEqual(0, productsInBasket?[100]);
+            Assert.AreEqual(0, productsInBasket?[11]);
         }
     }
 
