3d60474f90285710fa904166cc89746d737b8129
Kfir Nissim <kfirniss@post.bgu.ac.il>
2024-06-26 18:02:50 +0300

    create productDTO

--- MarketBackend/DAL/DTO/StoreDTO.cs
+++ MarketBackend/DAL/DTO/StoreDTO.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using MarketBackend.Domain.Models;
 using MarketBackend.Domain.Market_Client;
+using Market.DataLayer.DTOs;
 
 namespace MarketBackend.DAL.DTO
 {
