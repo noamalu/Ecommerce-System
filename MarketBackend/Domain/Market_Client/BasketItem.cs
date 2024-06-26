82c9b9a116072974a2b0ceb8796300d996504315
Kfir Nissim <kfirniss@post.bgu.ac.il>
2024-06-26 21:41:25 +0300

    use dto in basketItem is domains

--- MarketBackend/Domain/Market_Client/BasketItem.cs
+++ MarketBackend/Domain/Market_Client/BasketItem.cs
@@ -1,3 +1,6 @@
+using MarketBackend.DAL;
+using MarketBackend.DAL.DTO;
+
 namespace MarketBackend.Domain.Market_Client{
     public class BasketItem{
         private Product _product;
@@ -18,6 +21,28 @@ namespace MarketBackend.Domain.Market_Client{
             _product = product;
             _priceAfterDiscount = priceAfterDiscount;
             _quantity = quantity;
+        }
+
+        public BasketItem(PurchasedItemDTO purchasedItemDTO)
+        {
+            _product = ProductRepositoryRAM.GetInstance().GetById(purchasedItemDTO.ProductId);
+            _priceAfterDiscount = purchasedItemDTO.PriceAfterDiscount;
+            _quantity = purchasedItemDTO.Quantity;
+        }
+
+        public BasketItem(BasketItemDTO basketItemDTO)
+        {
+            _product = ProductRepositoryRAM.GetInstance().GetById(basketItemDTO.Product.ProductId);
+            _priceAfterDiscount = basketItemDTO.PriceAfterDiscount;
+            _quantity = basketItemDTO.Quantity;
+        }
+
+        internal BasketItem Clone()
+        {
+            Product productCoppy = _product.Clone();
+            double priceAfterDiscount = _priceAfterDiscount;
+            int quantity = _quantity;
+            return new BasketItem(productCoppy,quantity,priceAfterDiscount);
         }
 
 
