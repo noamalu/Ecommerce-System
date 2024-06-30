38a665bcdd6b1f920e884ffaaa28f4e9a5e0f1b9
ol-ozn <ol.oznovich@gmail.com>
2024-06-30 22:39:44 +0300

    added comments in conflicting files

--- MarketBackend/DAL/DTO/BasketDTO.cs
+++ MarketBackend/DAL/DTO/BasketDTO.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using MarketBackend.Domain.Market_Client;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using System.ComponentModel.DataAnnotations;
+//using MarketBackend.Domain.Market_Client;
 
 
-namespace MarketBackend.DAL.DTO
-{
-    [Table("Baskets")]
-    public class BasketDTO
-    {
-        [Key]
-        public int _basketId { get; set; }
-        [Required]
-        [ForeignKey("ShopDTO")]
-        public int _storeId { get; set; }
-        [Required]
-        [ForeignKey("CartDTO")]
-        public int _cartId {get; set;}
-        public List<BasketItemDTO> BasketItems { get; set; }
-        public Dictionary<int, int> products { get; set; }
+//namespace MarketBackend.DAL.DTO
+//{
+//    [Table("Baskets")]
+//    public class BasketDTO
+//    {
+//        [Key]
+//        public int _basketId { get; set; }
+//        [Required]
+//        [ForeignKey("ShopDTO")]
+//        public int _storeId { get; set; }
+//        [Required]
+//        [ForeignKey("CartDTO")]
+//        public int _cartId {get; set;}
+//        public List<BasketItemDTO> BasketItems { get; set; }
+//        public Dictionary<int, int> products { get; set; }
 
 
-        public BasketDTO(int shopId, List<BasketItemDTO> basketItems)
-        {
-            _storeId = shopId;
-            BasketItems = basketItems;
-        }
+//        public BasketDTO(int shopId, List<BasketItemDTO> basketItems)
+//        {
+//            _storeId = shopId;
+//            BasketItems = basketItems;
+//        }
 
-        public double TotalPrice { get; set; }
-        public BasketDTO() { }
-        public BasketDTO(Basket basket) {
-            _basketId = basket._basketId;
-            _storeId = basket._storeId;
-            _cartId = basket._cartId;
-            BasketItems = new List<BasketItemDTO>();
-            foreach (BasketItem item in basket.BasketItems)
-                BasketItems.Add(new BasketItemDTO(item));
-            products = basket.products.ToDictionary(entry => entry.Key,
-                                               entry => entry.Value);
+//        public double TotalPrice { get; set; }
+//        public BasketDTO() { }
+//        public BasketDTO(Basket basket) {
+//            _basketId = basket._basketId;
+//            _storeId = basket._storeId;
+//            _cartId = basket._cartId;
+//            BasketItems = new List<BasketItemDTO>();
+//            foreach (BasketItem item in basket.BasketItems)
+//                BasketItems.Add(new BasketItemDTO(item));
+//            products = basket.products.ToDictionary(entry => entry.Key,
+//                                               entry => entry.Value);
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
