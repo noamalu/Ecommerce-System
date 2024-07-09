3d39394a02ab757e546235ee2fae9ab8e853683c
Shaked Matityahu <102467439+shakedmatityahu@users.noreply.github.com>
2024-07-09 13:53:14 +0300

    .

--- MarketBackend/Domain/Market_Client/Member.cs
+++ MarketBackend/Domain/Market_Client/Member.cs
@@ -35,7 +35,7 @@ namespace MarketBackend.Domain.Market_Client
             OrderHistory = new(); 
             IsSystemAdmin = false;
             IsLoggedIn = false;
-            IsNotification = true; //TODO maybe change to true
+            IsNotification = false; //TODO maybe change to true
             alerts = new SynchronizedCollection<Message>();
         }
 
